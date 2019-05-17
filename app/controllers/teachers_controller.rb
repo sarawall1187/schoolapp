1cc236d6feab770fc867439ae764c51d48beb1d5
Sara Walker <sarawall1187@gmail.com>
2019-05-17 02:41:09 +0000

    added new teacher form and list of teachers to school show page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,9 +1,19 @@
 class TeachersController < ApplicationController
   def new
-    if params[:school_id] && @school = School.find_by_id(params[:school_id])
-        @teacher = @school.teachers.build
-    else
-      @teacher = Teacher.new
-    end
-end
+       if params[:school_id] && @school = School.find_by_id(params[:school_id])
+           @teacher = @school.teachers.build
+       else
+         @teacher = Teacher.new
+       end
+   end
+
+   def create
+       @school = School.find_by_id(params[:school_id])
+       @teacher = @school.teachers.build(teacher_params)
+       if @teacher.save
+           redirect_to school_path(@teacher.school_id)
+       else
+           render :new
+       end
+   end
 end
