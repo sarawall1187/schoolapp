2ca953fcfeb84956acb279b0ec761858b1a77532
sarawall1187 <sarawall1187@gmail.com>
2019-05-20 10:06:39 -0700

    created seeds and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  
+
+Teacher.create(name: 'Mrs. Smith', grade_taught: '1st', school_id: 1)
+Teacher.create(name: 'Mr. Bondwell', grade_taught: '3rd', school_id: 1)
+Teacher.create(name: 'Mrs. Jenkins', grade_taught: '4th', school_id: 1)
+Teacher.create(name: 'Mrs. Doubtfire', grade_taught: '5th', school_id: 1)
+Teacher.create(name: 'Mr. Greggs', grade_taught: '7th', school_id: 2)
+Teacher.create(name: 'Mrs. Clark', grade_taught: '8th', school_id: 2)
