e1c2f0da6b6827741a4bbfee451ff693eb6a0196
sarawall1187 <sarawall1187@gmail.com>
2019-07-06 12:06:19 -0700

    successfully injected all teachers into DOM

--- app/assets/javascripts/teachers.js
+++ app/assets/javascripts/teachers.js
@@ -7,7 +7,32 @@ const clickEventHandlers = () => {
       e.preventDefault()  
         fetch(`${this}/teachers.json`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(teachers => {
+                teachers.forEach((teacher) => {
+                    let newTeacher = new Teacher(teacher)
+                    let teacherHTML = newTeacher.formatIndex()
+                    $('#all_teachers').append(teacherHTML)
+                })
+            })
 
     })
 }
+
+
+function Teacher(teacher){
+    this.id = teacher.id
+    this.name = teacher.name
+    this.grade_taught = teacher.grade_taught
+    this.school = teacher.school
+    this.children = teacher.children
+}
+
+Teacher.prototype.formatIndex = function() {
+    let teacherHTML = `
+    <ul>
+   <li> <a>${this.name}, ${this.grade_taught}</a></li>
+   </ul>
+    `
+    return teacherHTML
+}
+    
